1b77bb146634cab40871ba643d0b0c2425d84ca5
scardummichele <scarpa.michele.90@gmail.com>
2021-08-01 19:19:45 +0200

    first component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import HomeDiary from './modules/diary/components/Home/HomeDiary';
 import './App.css';
 
 function App() {
@@ -18,6 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <HomeDiary></HomeDiary>
+      </div>
     </div>
   );
 }
---
+++ src/modules/diary/components/Home/HomeDiary.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class HomeDiary extends Component {
+    render() {
+        return ( 
+        <div>CIAO</div>
+        );
+    }
+}
+
+export default HomeDiary;
