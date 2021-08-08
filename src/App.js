9919e3b9a1a82305d05fe463da383317fd138349
scarpa.michele <scarpa.michele.90@gmail.com>
2021-08-08 14:47:18 +0200

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="container mx-auto px-10 h-5/6">
       <div className="bg-gradient-to-r from-blue-200 via-blue-100 to-blue-300 ...">
         <p>
-          Headache Diary          
+          Headache Diary   ciao       
         </p>
         <HeadacheModule></HeadacheModule>        
       </div>      
